a970e52ab7cc2a977a9db448664565c77ec8ac9f
Anna Hammer <117043167+anna-hammer@users.noreply.github.com>
2023-03-13 18:47:16 -0400

    Add files via upload

--- ProjectService.java
+++ ProjectService.java
@@ -1,6 +1,9 @@
 package projects.service;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import projects.dao.ProjectDao;
 import projects.entity.Project;
 
@@ -10,32 +13,51 @@ public class ProjectService {
 	
 	
 	
-	public static class projectDao {
+	private static final Integer ProjectId = null;
+	private ProjectDao projectDao = new ProjectDao();
 
-		public static Project insertProject(Project project) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
+	public static Project insertProject(Project project) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public Project addProject(Project project) {
-		return projectDao.insertProject(project);
-		
-		
-	}
+
+
+public Project addProject(Project project) {
+	return projectDao.insertProject(project);
+	
+	
+}
 
 public List<Project> fetchAllProjects() {
-	return projectDao.fetchAllProjects();
-	
+return projectDao.fetchAllProjects();
+
 }
-	/**
-	 * This method calls the project DAO to get all project details, including materials, steps, and categories.
-	 * 
-	 * @param projectId The Project ID.
-	 * @return A project object if successful.
-	 * @throws NoSuchElementExpectation Thrown if the project with the given ID does not exist.
-	 * 
-	 */
-	
+/**
+ * This method calls the project DAO to get all project details, including materials, steps, and categories.
+ * 
+ * @param projectId The Project ID.
+ * @return A project object if successful.
+ * @throws NoSuchElementExpectation Thrown if the project with the given ID does not exist.
+ * 
+ */
+
+Optional<Project> op = projectDao.fetchProjectById(ProjectId);
+
+
+public Project fetchProjectById(Integer projectId) {
+	return projectDao.fetchProjectById(projectId).orElseThrow();
 }
+
+
+
+public Project fetchAllProjectsById(Integer projectId) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+			
+}
+
+
+
